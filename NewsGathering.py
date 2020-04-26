c68d4559ac5f442240df2a6a33e13d84494493ec
adittandon83 <62414421+adittandon83@users.noreply.github.com>
2020-04-26 11:45:32 -0700

    Update NewsGathering.py

--- NewsGathering.py
+++ NewsGathering.py
@@ -2,7 +2,6 @@ from newsapi import NewsApiClient
 
 if __name__ == '__main__':
     newsapi = NewsApiClient(api_key='<yourAPIKEY>')
-    # top_headlines = newsapi.get_top_headlines(q='bitcoin', language='en')
     topic= input("Enter your topic of interest:")
     everything = newsapi.get_everything(qintitle=topic, language='en')
     num =1
